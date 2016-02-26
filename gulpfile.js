e0ed740c2711412642407778eadac6a6669772e0
Ferdi Pretorius <ferdinandpretorius@gmail.com>
2016-02-26 11:22:26 +0000

    Added serve and dist to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,55 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+     del = require('del');
 
 gulp.task("concatScripts", function() {
   'use strict';
-  gulp.src([
+  return gulp.src([
     'public/javascripts/jquery.js',
     'public/javascripts/main.js'])
   .pipe(concat("scripts.js"))
   .pipe(gulp.dest("public/javascripts"));
 });
 
-gulp.task("minifyScripts", function(){
+gulp.task("minifyScripts", ["concatScripts"], function(){
   'use strict';
-  gulp.src("public/javascripts/scripts.js")
+  return gulp.src("public/javascripts/scripts.js")
   .pipe(uglify())
   .pipe(rename('scripts.min.js'))
   .pipe(gulp.dest("public/javascripts"));
 });
 
-gulp.task('compileSass', function(){
-  gulp.src("public/stylesheets/scss/application.scss")
+gulp.task("compileSass", function(){
+  'use strict';
+  return gulp.src("public/stylesheets/scss/application.scss")
     .pipe(sass())
     .pipe(gulp.dest("public/stylesheets/css"));
 });
 
-gulp.task("default", ["hello"], function(){
-  console.log("This is the default task.");
+gulp.task("watchFiles", function(){
+  gulp.watch("public/stylesheets/scss/**/*.scss", ['compileSass']);
+  gulp.watch("public/javascripts/main.js", ['concatScripts']);
+});
+
+gulp.task('clean', function(){
+  del(['dist',
+       'public/stylesheets/css/application.css*',
+       'public/javascripts/scripts*.js*']);
+});
+
+gulp.task("build", ['minifyScripts', 'compileSass'], function(){
+  return gulp.src(["public/stylesheets/css/**",
+                   "public/javascripts/scripts.min.js",
+                   "views/*.jade",
+                   "public/images/**",
+                   "public/fonts/**"], { base: './' })
+                   .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ["clean"], function(){
+  gulp.start('build');
 });
