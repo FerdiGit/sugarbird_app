363301abafbf462655df80f63b8aea9fde3095d6
Ferdinand Pretorius <ferdinandpretorius@gmail.com>
2016-03-02 20:15:09 +0000

    Setup nodemailer for contact form

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
@@ -11,14 +17,42 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/contact', function(req, res){
+// Handle form data
+app.post('/', function(req, res){
   'use strict';
-  res.render('contact');
-});
+  var mailOpts, smtpTrans;
 
-app.get('/gallery', function(req, res){
-  'use strict';
-  res.render('gallery');
+  // Setup Nodemailer transport.
+  // Create an application-specific password to avoid problems
+  smtpTrans = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: "sugarbirdmail@gmail.com",
+      pass: "" // Change me before deployment
+    }
+  });
+
+  // Mail options
+  mailOpts = {
+    from: req.body.name + '&lt;' + req.body.email + '&gt;',
+    to: 'sugarbirdgfbaking@gmail.com',
+    subject: 'Website contact form',
+    text: "Name: " + req.body.name +
+          "\nEmail: " + req.body.email +
+          "\nMessage: "+ req.body.message
+  };
+
+  smtpTrans.sendMail(mailOpts, function(error, info){
+    // Email not sent
+    if (error) {
+      res.render('contact', { msg: 'Error occured, message not sent.',
+                              err: true, page: 'contact' });
+    } else {
+      console.log('message Sent'+info.response);
+      res.render('contact', { msg: 'Message sent! Thank you.',
+                              err: false, page: 'contact'});
+    }
+  });
 });
 
 app.listen(3000, function(){
